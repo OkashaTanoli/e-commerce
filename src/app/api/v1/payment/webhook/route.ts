86bcdea500e0c50020ac696a46c418f129e27cc9
okashatanoli12345@gmail.com <64710012+OkashaTanoli@users.noreply.github.com>
2023-06-07 02:46:47 +0500

    test

--- src/app/api/v1/payment/webhook/route.ts
+++ src/app/api/v1/payment/webhook/route.ts
@@ -10,7 +10,7 @@ const endpointSecret = process.env.ENDPOINT_SECRET!;
 // export const config = { api: { bodyParser: false } }
 
 export async function POST(request: NextRequest) {
-    console.log("hello man")
+    // console.log("hello man")
     const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, { apiVersion: '2022-11-15' })
 
     const sig = request.headers.get('stripe-signature')!;
@@ -20,13 +20,13 @@ export async function POST(request: NextRequest) {
     try {
         event = stripe.webhooks.constructEvent(data, sig, endpointSecret);
     } catch (err: any) {
-        console.log("error =========================>>>>>>>>>>>>>>>>>>>>>>>>", err)
+        console.log(err)
         return NextResponse.json(`Webhook Error: ${err.message}`)
     }
 
     if (event.type === 'checkout.session.completed') {
         const paymentIntentSucceeded = event.data.object;
-        console.log("paymentIntentSucceeded =====================>>>>>>>>>>>>>>>>> ", paymentIntentSucceeded)
+        console.log(paymentIntentSucceeded)
     }
     return NextResponse.json({ message: "done" })
 }
