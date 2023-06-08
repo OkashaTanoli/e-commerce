2042b2e7c9b43b0b8de4e749619983c5961d1187
okashatanoli12345@gmail.com <64710012+OkashaTanoli@users.noreply.github.com>
2023-06-08 15:03:16 +0500

    change request body to stringify

--- src/app/api/v1/payment/webhook/route.ts
+++ src/app/api/v1/payment/webhook/route.ts
@@ -13,39 +13,39 @@ export async function POST(request: NextRequest) {
     console.log(process.env.STRIPE_SECRET_KEY)
     const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, { apiVersion: '2022-11-15' })
     const sig = request.headers.get('stripe-signature')!;
-    const data = await request.text()
+    const data = await request.json()
     console.log("Signature ==================== >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>", sig)
-    console.log("Data ==================== >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>", data)
+    console.log("Data ==================== >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>", JSON.stringify(data))
     console.log("EndPoint ==================== >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>", endpointSecret)
     let event;
-    // try {
-    // event = stripe.webhooks.constructEvent(data, sig, endpointSecret);
-    // } catch (err: any) {
-    //     console.log("ERROR================>>>>>>>>>>", err)
-    //     return NextResponse.json(`Webhook Error =======>>>>>>>>>> : ${err.message}`)
-    // }
+    try {
+        event = stripe.webhooks.constructEvent(JSON.stringify(data), sig, endpointSecret);
+    } catch (err: any) {
+        console.log("ERROR================>>>>>>>>>>", err)
+        return NextResponse.json(`Webhook Error =======>>>>>>>>>> : ${err.message}`)
+    }
 
-    // if (event.type === 'checkout.session.completed') {
-    //     try {
+    if (event.type === 'checkout.session.completed') {
+        try {
 
-    //         const data: any = event.data.object;
-    //         const customer: any = await stripe.customers.retrieve(data.customer)
-    //         // const listLineItems = await stripe.checkout.sessions.listLineItems(data.id)
-    //         let newOrder: NewOrder = {
-    //             id: uuidv4(),
-    //             subtotal: data.amount_subtotal,
-    //             total: data.amount_total,
-    //             payment_intent_id: data.payment_intent,
-    //             payment_status: data.payment_status,
-    //             products: JSON.stringify(Object.values(customer.metadata).map((val: any) => JSON.parse(val))),
-    //             shipping: JSON.stringify(data.customer_details),
-    //         }
-    //         const orders = await db.insert(Orders).values(newOrder).returning();
-    //         console.log(orders)
-    //     }
-    //     catch (err) {
-    //         console.log("error ========>>>>>>>>>", err)
-    //     }
-    // }
+            const data: any = event.data.object;
+            const customer: any = await stripe.customers.retrieve(data.customer)
+            // const listLineItems = await stripe.checkout.sessions.listLineItems(data.id)
+            let newOrder: NewOrder = {
+                id: uuidv4(),
+                subtotal: data.amount_subtotal,
+                total: data.amount_total,
+                payment_intent_id: data.payment_intent,
+                payment_status: data.payment_status,
+                products: JSON.stringify(Object.values(customer.metadata).map((val: any) => JSON.parse(val))),
+                shipping: JSON.stringify(data.customer_details),
+            }
+            const orders = await db.insert(Orders).values(newOrder).returning();
+            console.log(orders)
+        }
+        catch (err) {
+            console.log("error ========>>>>>>>>>", err)
+        }
+    }
     return NextResponse.json({ message: "done" })
 }
