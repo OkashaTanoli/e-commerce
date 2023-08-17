c9ffa1b7323d7f9ea3a9b6cad5ccf9e95ee874c9
okashatanoli12345@gmail.com <64710012+OkashaTanoli@users.noreply.github.com>
2023-08-18 04:19:07 +0500

    deleted checking of stripe webhook

--- src/app/api/v1/payment/webhook/route.ts
+++ src/app/api/v1/payment/webhook/route.ts
@@ -20,13 +20,13 @@ export async function POST(request: NextRequest) {
     // console.log("Data ==================== >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>", JSON.stringify(data))
     console.log("EndPoint ==================== >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>", endpointSecret)
     let event;
-    try {
-        event = stripe.webhooks.constructEvent(data, sig, endpointSecret);
-        console.log("SUCCESSSSSSSSSSSSSS================>>>>>>>>>>")
-    } catch (err: any) {
-        console.log("ERROR================>>>>>>>>>>", err)
-        return NextResponse.json(`Webhook Error =======>>>>>>>>>> : ${err.message}`)
-    }
+    // try {
+    event = stripe.webhooks.constructEvent(data, sig, endpointSecret);
+    //     console.log("SUCCESSSSSSSSSSSSSS================>>>>>>>>>>")
+    // } catch (err: any) {
+    //     console.log("ERROR================>>>>>>>>>>", err)
+    //     return NextResponse.json({ messahe: `Webhook Error =======>>>>>>>>>> : ${err.message}` }, { status: 500 })
+    // }
 
     if (event.type === 'checkout.session.completed') {
         try {
